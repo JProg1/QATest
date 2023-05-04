b73ff3a61f2aac10f2dffd9358d977c8b9aefbd9
JCottonD <131383936+JCottonD@users.noreply.github.com>
2023-05-04 11:25:17 +0100

    Initial commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,9 @@
+function AboutPage() {
+    return (
+        <div className="aboutPage">
+            <h2>About Page</h2>
+            <p>Lorem</p>
+        </div>
+    );
+}
+export default AboutPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div className="homePage">
+            <h2>This is the homepage.</h2>
+            <img src="https://user-images.githubusercontent.com/118732445/203775030-f6d39588-6b73-42eb-a396-d50098078501.jpg"/>
+        </div>
+    );
+}
+export default HomePage;
